a99a7661affb719f78ff3c2f2b72a5ab9bd810ac
nmsoikat <nursaykat@gmail.com>
2022-01-22 10:18:25 +0600

    use dummyData for posts and users

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,31 +1,33 @@
 import { MoreVert } from '@mui/icons-material';
 import './post.css'
+import { Users } from '../../dummyData';
 
-export default function () {
+export default function ({ post }) {
+
   return <div className='post'>
     <div className="post-wrapper">
       <div className="post-top">
         <div className="post-top-left">
-          <img  className='post-profile-img' src="/assets/person/7.jpeg"/>
-          <span className="post-username">Nur Mohammad</span>
-          <span className="post-date">5 mins ago</span>
+          <img className='post-profile-img' src={Users.filter(u => u.id === post.userId)[0].profilePicture} />
+          <span className="post-username">{Users.filter(u => u.id === post.userId)[0].username}</span>
+          <span className="post-date">{post.date}</span>
         </div>
         <div className="post-top-right">
-          <MoreVert className='post-option-icon'/>
+          <MoreVert className='post-option-icon' />
         </div>
       </div>
       <div className="post-center">
-        <span className="post-text">Hey it is my first post</span>
-        <img className='post-img' src="/assets/post/1.jpeg"/>
+        <span className="post-text">{post?.desc}</span>
+        <img className='post-img' src={post.photo} />
       </div>
       <div className="post-bottom">
         <div className="post-bottom-left">
           <img className='post-like-icon' src="/assets/like.png" alt="like" />
           <img className='post-like-icon' src="/assets/heart.png" alt="heart" />
-          <span className="post-like-count">32 people like it</span>
+          <span className="post-like-count">{post.like} people like it</span>
         </div>
         <div className="post-bottom-right">
-          <span className="post-comment-text">9 comments</span>
+          <span className="post-comment-text">{post.comment} comments</span>
         </div>
       </div>
     </div>
